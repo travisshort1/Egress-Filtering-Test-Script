2905245b8feea91a86a792da18ae25071ce09266
Travis Short <63628648+travisshort1@users.noreply.github.com>
2024-10-17 11:52:49 -0400

    Add files via upload

---
+++ egress_testing.ps1
@@ -0,0 +1,13 @@
+﻿# Define the target host and ports to test
+$targetHost = (resolve-dnsname -Type A open.zorinaq.com ).IPAddress
+$ports = @(2..19)  # Add your list of ports here
+
+# Loop through each port and test the connection
+foreach ($port in $ports) {
+    $result = Test-NetConnection -ComputerName $targetHost -Port $port
+    if ($result.TcpTestSucceeded) {
+        Write-Host "Port ${port}: Connection to $targetHost succeeded." -ForegroundColor Green
+    } else {
+        Write-Host "Port ${port}: Connection to $targetHost failed." -ForegroundColor Red
+    }
+}
